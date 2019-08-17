810c86e7484fe147523824e73f1fe3868caf27c1
Yuriy <Seaman_on_watch@mail.ru>
2019-08-17 22:47:03 +0300

    Working with Class and Object

---
+++ CSharpBasic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    class Program
+    {
+        //A label for the class program
+        //Program p = new Program();
+        static void Main(string[] args)
+        {
+            TestClass1 class1 = new TestClass1();
+            class1.SetValue(20);
+            class1.GetValue();
+            //class1.TestCase1("PASS");
+            //class1.TestCase2("FAILED");
+            //Console.Write("Hello, world");
+            TestClass1 class2 = new TestClass1();
+            class2.SetValue(30);
+            class2.GetValue();
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpBasic/TestClass1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    class TestClass1
+    {
+        private int i = 0;
+        public void SetValue(int value)
+        {
+            i = value;
+        }
+        public void GetValue()
+        {
+            Console.WriteLine("The value of i is : " + i);
+        }
+        //Members
+        public void TestCase1(string result)
+        {
+            Console.WriteLine("executing TestCase 1 "
+                + result);
+        }
+
+        public void TestCase2(string result)
+        {
+            Console.WriteLine("executing TestCase 2 "
+                + result);
+        }
+
+    }
+}
