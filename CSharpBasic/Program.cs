0207abc7503a205f141a322c6fbbd6610e31c3d5
Yuriy <Seaman_on_watch@mail.ru>
2019-09-21 19:57:07 +0300

    C# 7.0 new feature - Pattern matching in Case Block

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -2,6 +2,7 @@
 using CSharpBasic.Delegates;
 using CSharpBasic.ExtensionMethods;
 using CSharpBasic.Selenium;
+using CSharpBasic.Sharp7;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,51 +26,52 @@ namespace CSharpBasic
         static void Main ( string[] args )
             {
 
-            //C#3.5
-            Func<string, string> p = delegate ( string values )
-                 {
-                     return values;
-                     };
+            NewFeatures feature = new NewFeatures ();
 
-            Console.WriteLine ($"The value from Func<> delegates is: {p ("executeautomation")}");
-
-            Action<string> actionP = delegate ( string values )
-                {
-                    Console.WriteLine ($"The value from Action<> is: {values}");
-                    };
-
-            Action<string> actionWithLambda = values =>
-            {
-                Console.WriteLine ($"The value from Action<> Lambda is: {values}");
-            };
-
-            actionP ("executeautomation");
-            actionWithLambda ("executeautomation");
-
-
-
+            feature.GetStudentsInfoWithGrade (feature.student.First ());
             //Console.WriteLine (GetBrowserName (Browser.IE));
             //IWebDriver driver = new Chrome ();
             //driver.FindElement ();
             //driver.SendKeysWithSpChar ("executeautomation", "!!!");
             //CollectionsExample.GenericCollectionsWithCustomClass ();
 
-            //Delegate examples C#1.0
+            //      Delegate examples C#1.0
             //Printer p = DelegateTry.PrintValue_1;
             //p ("executeautomation");
 
-            //Delegate examples C#2.0
+            //      Delegate examples C#2.0
             //Printer p = delegate ( string values )
             //    {
             //        Console.WriteLine ($"The value printed inline: {values}");
             //        };
             //p ("executeautomation");
 
-            //Delegate examples C#3.0
+            //      Delegate examples C#3.0
             //Printer p = ( values ) => { Console.WriteLine ($"The value printed from Lambda expression: {values}"); };
             //p ("executeautomation");
-            Console.Read ();
 
+            //      C#3.5
+            //Func<string, string> p = delegate ( string values )
+            //    {
+            //        return values;
+            //        };
+
+            //Console.WriteLine ($"The value from Func<> delegates is: {p ("executeautomation")}");
+
+            //Action<string> actionP = delegate ( string values )
+            //    {
+            //        Console.WriteLine ($"The value from Action<> is: {values}");
+            //        };
+
+            //Action<string> actionWithLambda = values =>
+            //{
+            //    Console.WriteLine ($"The value from Action<> Lambda is: {values}");
+            //};
+
+            //actionP ("executeautomation");
+            //actionWithLambda ("executeautomation");
+
+            Console.Read ();
             }
 
         public static string GetBrowserName ( Browser browser )
---
+++ CSharpBasic/Sharp7/NewFeatures.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic.Sharp7
+    {
+    public class NewFeatures
+        {
+
+        public List<Students> student = null;
+
+        public NewFeatures ()
+            {
+            var s = new List<Students>
+                {
+                new Students()
+                    {
+                    Name = "Karthik",
+                    Age = 20,
+                    Gender = "M",
+                    Grade = "First"
+                    },
+                new Students()
+                    {
+                    Name = "Prashanth",
+                    Age = 25,
+                    Gender = "M",
+                    Grade = "First"
+                    },
+                new Students()
+                    {
+                    Name = "Shrea",
+                    Age = 30,
+                    Gender = "F",
+                    Grade = "Second"
+                    }
+                };
+            student=s;
+            }
+        public void GetStudentsInfoWithGrade ( Students student )
+            {
+            switch (student)
+                {
+                case Students s when (s.Grade=="First"&&s.Name.Contains ("Kart")):
+                    Console.WriteLine ($"Student with name {s.Name} is with Age {s.Age}");
+                    break;
+                case Students s when (s.Grade=="Second"):
+                    Console.WriteLine ($"Student with name {s.Name} is with Age {s.Age}");
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+
+    public class Students
+        {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public string Gender { get; set; }
+        }
+    }
